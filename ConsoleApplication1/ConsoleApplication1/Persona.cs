6a34d3b325ced404628a13465000b29ff3fbaaa9
Luca <36246877+xTomluca@users.noreply.github.com>
2018-07-04 20:52:09 -0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Persona
+    {
+        private int edad;
+        String nombre;
+        public int setEdad(int edad){
+            this.edad = edad;
+            return 0;
+        }
+        public int getEdad() {
+            return this.edad;
+        }
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+        public string getNombre() {
+            return this.nombre;
+        }
+        
+            
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona aux = new Persona();
+            int edadT;
+            String nombreT;
+            aux.setNombre("Josesito");
+            aux.setEdad(int.Parse(Console.ReadLine()));
+            Console.Read();
+            edadT = aux.getEdad();
+            nombreT = aux.getNombre();
+            Console.Read();
+            Console.WriteLine("La edad es "+edadT+" y su nombre: "+nombreT);
+            Console.Read();
+            
+        }
+    }
+}
